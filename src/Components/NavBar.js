adb2c9826dd84f8181d8ff6239270a01a5bad71c
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-01-02 15:39:26 +0100

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,6 @@ import { NavLink } from 'react-router-dom';
 import logo from '../Images/planet.png';
 
 const NavBar = () => {
-  // This styling will be applied to a <NavLink> when the
-  // route that it links to is currently selected.
   const activeStyle = {
     textDecoration: 'underline',
   };
