7355e7e6fc6c542f87295eb7741155e536f6bb7e
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-01-03 20:37:27 +0100

    Add: Styling

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,21 +1,49 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectAllRockets, fetchRockets } from '../Redux/rockets/rocketsSlice';
+
 import './rocket.css';
 
 const Rockets = () => {
-  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  const rockets = useSelector(selectAllRockets);
 
-  const renderedRockets = rockets.map((rocket) => (
-    <article key={rocket.id}>
-      <img className="rocket-img" src={rocket.flickr_images[0]} alt={rocket.name} />
-      <h1>{rocket.name}</h1>
-      <p>{rocket.description}</p>
-    </article>
-  ));
+  const rocketStatus = useSelector((state) => state.rockets.status);
+  const error = useSelector((state) => state.rockets.error);
 
+  useEffect(() => {
+    if (rocketStatus === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [rocketStatus, dispatch]);
+
+  let content;
+  const [reserved, setReservation] = useState(false);
+  const flip = () => {
+    setReservation(!reserved);
+  };
+
+  if (rocketStatus === 'loading') {
+    content = <div className="loader">Loading...</div>;
+  } else if (rocketStatus === 'succeeded') {
+    content = rockets.map((rocket) => (
+      <article key={rocket.id}>
+        <img className="rocket-img" src={rocket.flickr_images[0]} alt={rocket.name} />
+        <div className="rocket-details">
+          <h1>{rocket.rocket_name}</h1>
+          <p>{rocket.description}</p>
+          {reserved
+            ? <button onClick={() => flip()} key={rocket.id} className="reserve-btn" type="button">Reserve Rocket</button>
+            : <button onClick={() => flip()} key={rocket.id} className="reserved" type="button">Cancel Reservation</button>}
+        </div>
+      </article>
+    ));
+  } else if (rocketStatus === 'failed') {
+    content = <div>{error}</div>;
+  }
   return (
     <section className="rockets-list">
-      <div>Rockets</div>
-      {renderedRockets}
+      {content}
     </section>
   );
 };
--- src/Redux/rockets/rocketsSlice.js
+++ src/Redux/rockets/rocketsSlice.js
@@ -1,47 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = [
-  {
-    id: 1,
-    name: 'Falcon 1',
-    description: 'Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009',
-    flickr_images: [
-      'https://farm9.staticflickr.com/8166/7121865553_e863f1e7c0_b.jpg',
-      'https://farm1.staticflickr.com/33/45336904_1aef569b30_o.jpg',
-    ],
-    wikipedia: 'https://en.wikipedia.org/wiki/Dragon_SpaceX',
-  },
-  {
-    id: 2,
-    name: 'Falcon 9',
-    description: 'Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit.',
-    flickr_images: [
-      'https://farm2.staticflickr.com/1466/23898653041_3dd203fe1b_b.jpg',
-      'https://farm2.staticflickr.com/1562/25125530524_02f8c2a5c3_b.jpg',
-      'https://farm2.staticflickr.com/1627/25125530864_0c4b0f3d42_b.jpg',
-      'https://farm2.staticflickr.com/1661/25189273053_0a7c2d4a6f_b.jpg',
-      'https://farm2.staticflickr.com/1639/25189273113_5f0a3a9d5a_b.jpg',
-    ],
-    wikipedia: 'https://en.wikipedia.org/wiki/Falcon_9',
-  },
-  {
-    id: 3,
-    name: 'Falcon Heavy',
-    description: 'Falcon Heavy is a heavy-lift launch vehicle designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit.',
-    flickr_images: [
-      'https://farm2.staticflickr.com/1417/1430198323_c26451b047_b.jpg',
-      'https://farm2.staticflickr.com/1461/24340510689_646f3a7b68_b.jpg',
-      'https://farm2.staticflickr.com/1442/23898653011_b3f7c725f2_b.jpg',
-      'https://farm2.staticflickr.com/1488/24340510919_5e64c538be_b.jpg',
-    ],
-    wikipedia: 'https://en.wikipedia.org/wiki/Falcon_Heavy',
-  },
-];
+const GET_ROCKETS = 'spacetravellershub/rockets/fetchRockets';
+
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: null,
+};
+
+export const fetchRockets = createAsyncThunk(GET_ROCKETS, async () => {
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  return response.json();
+});
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = state.rockets.concat(action.payload);
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default rocketsSlice.reducer;
+
+export const selectAllRockets = (state) => state.rockets.rockets;
