68d7036c169ee7e8229f42e2a0526f52f868bedf
asharanjith <ashasvijayan@gmail.com>
2023-01-03 23:00:15 +0400

    Add basic structure of profile page and its styles

--- src/Pages/MyProfile.js
+++ src/Pages/MyProfile.js
@@ -1,5 +1,25 @@
+import React from 'react';
+import './MyProfile.css';
+
 const MyProfile = () => (
-  <div>MyProfile</div>
+  <div className="profile">
+    <div className="myContainer">
+      <div className="heading">My Missions</div>
+      <ul className="profileList">
+        <li>Mission 1</li>
+        <li>Mission 2</li>
+        <li>Mission 3</li>
+      </ul>
+    </div>
+    <div className="myContainer">
+      <div className="heading">My Rockets</div>
+      <ul className="profileList">
+        <li>Rocket 1</li>
+        <li>Rocket 2</li>
+        <li>Rocket 3</li>
+      </ul>
+    </div>
+  </div>
 );
 
 export default MyProfile;
