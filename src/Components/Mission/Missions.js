ef9eed394d4554a88dc293698f405fd976f60729
asharanjith <ashasvijayan@gmail.com>
2023-01-03 20:00:05 +0400

    Add functionalities to the buttons

--- src/Components/Mission/Missions.js
+++ src/Components/Mission/Missions.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../../Redux/Mission/mission';
 import './mission.css';
 
 const Mission = ({
-  name, description, status, index,
+  name, description, index, join, keyValue,
 }) => {
+  const dispatch = useDispatch();
   const odds = index % 2;
   const styles = {
     backgroundColor: odds ? 'rgb(182, 180, 180, 0.5)' : '#ffffff',
@@ -14,9 +17,23 @@ const Mission = ({
       <div className="mission-details" style={styles}>
         <div className="mission-name">{name}</div>
         <div className="mission-description">{description}</div>
-        <div className="mission-status">{status}</div>
+        <div className="mission-status-container">
+          {join
+            ? (<span className="mission-status-Active">Active Member</span>)
+            : (<span className="mission-status">NOT A MEMBER</span>) }
+        </div>
         <div className="mission-button">
-          <button type="button">Join Mission</button>
+          {join
+            ? (
+              <button type="button" className="leaveMission" onClick={() => dispatch(joinMission(keyValue))}>
+                Leave Mission
+              </button>
+            )
+            : (
+              <button type="button" className="joinMission" onClick={() => dispatch(joinMission(keyValue))}>
+                Join Mission
+              </button>
+            )}
         </div>
       </div>
     </div>
@@ -26,7 +43,8 @@ const Mission = ({
 Mission.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
+  join: PropTypes.bool.isRequired,
+  keyValue: PropTypes.string.isRequired,
 };
 export default Mission;
--- src/Redux/Mission/mission.js
+++ src/Redux/Mission/mission.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const missions = [];
+let missionState;
 
 const GET_MISSIONS = 'SPACETRAVELLERSHUB/MISSION/GET_MISSIONS';
 const GET_MISSIONS_SUCCESS = 'SPACETRAVELLERSHUB/MISSION/GET_MISSIONS_SUCCESS';
+const MISSION_JOINED = 'SPACETRAVELLERSHUB/MISSION/MISSION_JOINED';
 
 const getMissionsSuccess = (missions) => ({
   type: GET_MISSIONS_SUCCESS,
@@ -12,6 +14,7 @@ const getMissionsSuccess = (missions) => ({
     mission_id: mission.mission_id,
     mission_name: mission.mission_name,
     description: mission.description,
+    join: false,
   })),
 });
 
@@ -23,10 +26,23 @@ export const getMissions = createAsyncThunk(
   },
 );
 
+export const joinMission = (missionId) => (
+  {
+    type: MISSION_JOINED,
+    mission_id: missionId,
+  });
+
 const missionsReducer = (state = missions, action) => {
   switch (action.type) {
     case GET_MISSIONS_SUCCESS:
       return [...action.payload];
+    case MISSION_JOINED:
+      missionState = state.map((mission) => {
+        if (mission.mission_id !== action.mission_id) { return mission; }
+        return { ...mission, join: !mission.join };
+      });
+      return missionState;
+
     default:
       return state;
   }
