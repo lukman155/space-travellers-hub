2823ab8f577efd45b629cc2843068cbdd1cbbbe6
asharanjith <ashasvijayan@gmail.com>
2023-01-03 23:20:12 +0400

    Add joined missions to the profile page

--- src/Pages/MyProfile.js
+++ src/Pages/MyProfile.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './MyProfile.css';
 
-const MyProfile = () => (
-  <div className="profile">
-    <div className="myContainer">
-      <div className="heading">My Missions</div>
-      <ul className="profileList">
-        <li>Mission 1</li>
-        <li>Mission 2</li>
-        <li>Mission 3</li>
-      </ul>
+const MyProfile = () => {
+  const joinedMissions = useSelector((state) => state.missions).filter((missions) => missions.join);
+  return (
+    <div className="profile">
+      <div className="myContainer">
+        <div className="heading">My Missions</div>
+        <ul className="profileList">
+          {joinedMissions.length > 0 ? (joinedMissions.map((mission) => (
+            <li key={mission.mission_id}>{mission.mission_name}</li>
+          )))
+            : (<p>No Missions Joined</p>)}
+        </ul>
+      </div>
+      <div className="myContainer">
+        <div className="heading">My Rockets</div>
+        <ul className="profileList">
+          <li>Rocket 1</li>
+          <li>Rocket 2</li>
+          <li>Rocket 3</li>
+        </ul>
+      </div>
     </div>
-    <div className="myContainer">
-      <div className="heading">My Rockets</div>
-      <ul className="profileList">
-        <li>Rocket 1</li>
-        <li>Rocket 2</li>
-        <li>Rocket 3</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default MyProfile;
